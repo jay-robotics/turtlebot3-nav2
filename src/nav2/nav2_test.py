40408115a5372c61bec0a996c1919cb5d53cd09b
Jay Gajjar <jaygajjar890@gmail.com>
2026-05-10 15:32:04 +0530

    Update code

--- src/nav2/nav2_test.py
+++ src/nav2/nav2_test.py
@@ -6,16 +6,18 @@ from geometry_msgs.msg import PoseStamped
 import tf_transformations
 
 def create_pose_stamped(navigator:BasicNavigator,position_x,position_y,orientation_z):
-    qx,qy,qy,qw=tf_transformations.quaternion_from_euler(0.0,0.0,orientation_z)
+    qx,qy,qz,qw=tf_transformations.quaternion_from_euler(0.0,0.0,orientation_z)
     pose=PoseStamped()
     pose.header.frame_id='map'
     pose.header.stamp=navigator.get_clock().now().to_msg()
     pose.pose.position.x=position_x
     pose.pose.position.y=position_y
     pose.pose.position.z=0.0
-    pose.pose.orientation.x=0.0
-    pose.pose.orientation.y=0.0
-    pose.pose.orientation.z=orientation_z
+    pose.pose.orientation.x=qx
+    pose.pose.orientation.y=qy
+    pose.pose.orientation.z=qz
+    pose.pose.orientation.w=qw
+    return pose
 
 def main():
     rclpy.init()
@@ -28,17 +30,22 @@ def main():
     # wait for nav2
     nav.waitUntilNav2Active()
 
-    # send nav2 goal
-    # pi=3.14=180
-    #pi/2=1.57=90
-    goal_pose=PoseStamped()
+   # nav2 poses
+    goal_pose1=create_pose_stamped(nav,3.5,1.0,1.57)
+    goal_pose2=create_pose_stamped(nav,2.0,2.5,3.14)
+    goal_pose3=create_pose_stamped(nav,0.5,1.0,-1.57)
 
-    goal_pose=create_pose_stamped(nav,2.5,1.0,1.57)
-    nav.goToPose(goal_pose)
+    # #go to one pose
+    # nav.goToPose(goal_pose1)
+    # while not nav.isTaskComplete():
+    #     feedback=nav.getFeedback()
+    #     print(feedback)
 
+    #follow waypoints
+    waypoints=[goal_pose1,goal_pose2,goal_pose3]
+    nav.followWaypoints(waypoints)
     while not nav.isTaskComplete():
-        feedback=nav.getFeedback()
-        print(feedback)
+        print(nav.getFeedback())
         
     print(nav.getResult())
 
