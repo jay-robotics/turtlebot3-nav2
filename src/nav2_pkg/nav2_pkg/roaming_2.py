6e2bce3fd9be2e5ae256344e8fb1cdb6a671fd04
Jay Gajjar <jaygajjar890@gmail.com>
2026-06-06 16:44:54 +0530

    added motion logic

--- src/nav2_pkg/nav2_pkg/roaming_2.py
+++ src/nav2_pkg/nav2_pkg/roaming_2.py
@@ -16,19 +16,23 @@ class Roaming_2(Node):
             self.publish_scan, #callbacn fn
             10
         )
+
+        self.publisher_=self.create_publisher(TwistStamped,"/cmd_vel",10)
         self.gap_threshold=1.0
         self.safe_gap_list=[]
+        
 
         
     
-    def publish_scan(self,msg:LaserScan):
+    def publish_scan(self,msg:LaserScan,cmd:TwistStamped):
             self.gap_list=[]
             self.safe_gap_list=[]
             temp_list=[]
                   
             self.front_window=msg.ranges[0:7]+msg.ranges[359:352:-1]
         
-            self.remaining=msg.ranges[7:353]
+            self.remaining=msg.ranges[0:360]
+            # self.remaining=msg.ranges[7:353]
             for index,ray in enumerate(self.remaining,start=7):
                 if math.isinf(ray):
                       ray=3.5
@@ -70,7 +74,7 @@ class Roaming_2(Node):
             for gap,average in zip(self.safe_gap_list,self.average_list):
                   start=gap[0][0]
                   end=gap[-1][0]
-                  print(f"gap of width {len(gap)} start:{start} end:{end} has average distance {average} ")
+                #   print(f"gap of width {len(gap)} start:{start} end:{end} has average distance {average} ")
             maxValue=max(self.average_list)
             maxValue_index=self.average_list.index(maxValue)
             print(f" max average value: {maxValue} at Index {maxValue_index}")
@@ -81,17 +85,81 @@ class Roaming_2(Node):
             self.middle_ray_index=width//2
 
 
-            self.middle_ray=safe_gap[self.middle_ray_index]
+            self.middle_ray=safe_gap[self.middle_ray_index]  #middle ray
                  
-            print(f"selected gap Start:{start_safe_gap} End:{end_safe_gap} Average:{maxValue} Width:{width} Middle value:{self.middle_ray} ")
+            # print(f"selected gap Start:{start_safe_gap} End:{end_safe_gap} Average:{maxValue} Width:{width} Middle value:{self.middle_ray} ")
             print("\n")
+            print(f"middle ray {self.middle_ray}")
+
+
+            counter=0
+            cmd=TwistStamped()
+            linear_speed=0.1
+            angular_speed=0.1
+            stop=0.0
+            self.obstacle_threshold=1.0
+            for i in self.front_window:
+                 if (i<self.obstacle_threshold or i==self.obstacle_threshold):
+                      counter+=1
+                      if counter==2:
+                           break
+                 elif i>self.obstacle_threshold:
+                      counter=0
+                     
+            if counter==2:
+                cmd.twist.linear.x=stop
+                print(f"counter value is 2 : finding gap")
+
+                m=self.middle_ray[0]
+                if (0<=m<180 or m==180):
+                    error=m
+                    print(f"Error {error}")
+                    if error!=0:
+                        cmd.twist.angular.z=angular_speed
+                        print("0<=m<180 or m==180 | error!=0 | Rotating left")
+                    elif error==0:
+                        cmd.twist.angular.z=stop
+                        print("0<=m<180 or m==180 | error=0 | stopping")
+            
+                elif (180<m<=360):
+                    error=m-360
+                    print(f"Error {error}")
+                    if error<0:
+                        cmd.twist.angular.z=-angular_speed
+                        print("180<m<=360 | error<0 | Rotating right")
+                    elif error==0:
+                        cmd.twist.angular.z=stop
+                        print("180<m<=360 | error==0 | stopping")
+            else:
+                cmd.twist.linear.x=linear_speed  
+                cmd.twist.angular.z=stop  
+                print("counter!=0 | moving")   
+
+
+                
+
+
+            self.publisher_.publish(cmd)
+
+           
+                 
+                
+                
+
+            
+
+
+            
+
+
+
             
                       
                  
             
 
 
-9
+
                 #  print(f"gap of width {len(gap)} has average distance {average} ")
             
 
