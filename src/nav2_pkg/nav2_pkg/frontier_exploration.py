ae4c474b70c787ae162fc7200bd511a03fd3b3c7
Jay Gajjar <jaygajjar890@gmail.com>
2026-06-15 22:57:55 +0530

    converted 1d occupancy grid to 2d

--- src/nav2_pkg/nav2_pkg/frontier_exploration.py
+++ src/nav2_pkg/nav2_pkg/frontier_exploration.py
@@ -36,41 +36,41 @@ class frontier_exploration(Node):
 
 
 
-        grid = self.two_d_grid
+        # grid = self.two_d_grid
 
-        window = 10
-        center_row = 50
-        center_col = 40
+        # window = 10
+        # center_row = 50
+        # center_col = 40
 
-        r_start = max(0, center_row - window)
-        r_end = min(len(grid), center_row + window + 1)
+        # r_start = max(0, center_row - window)
+        # r_end = min(len(grid), center_row + window + 1)
 
-        c_start = max(0, center_col - window)
-        c_end = min(len(grid[0]), center_col + window + 1)
+        # c_start = max(0, center_col - window)
+        # c_end = min(len(grid[0]), center_col + window + 1)
 
-        fig, ax = plt.subplots(figsize=(10, 10))
+        # fig, ax = plt.subplots(figsize=(10, 10))
 
-        for r in range(r_start, r_end):
-            for c in range(c_start, c_end):
-                ax.text(
-                    c - c_start + 0.5,
-                    r - r_start + 0.5,
-                    str(grid[r][c]),
-                    ha="center",
-                    va="center"
-                )
+        # for r in range(r_start, r_end):
+        #     for c in range(c_start, c_end):
+        #         ax.text(
+        #             c - c_start + 0.5,
+        #             r - r_start + 0.5,
+        #             str(grid[r][c]),
+        #             ha="center",
+        #             va="center"
+        #         )
 
-        for x in range(c_end - c_start + 1):
-            ax.axvline(x)
+        # for x in range(c_end - c_start + 1):
+        #     ax.axvline(x)
 
-        for y in range(r_end - r_start + 1):
-            ax.axhline(y)
+        # for y in range(r_end - r_start + 1):
+        #     ax.axhline(y)
 
-        ax.set_xlim(0, c_end - c_start)
-        ax.set_ylim(r_end - r_start, 0)
-        ax.set_aspect("equal")
+        # ax.set_xlim(0, c_end - c_start)
+        # ax.set_ylim(r_end - r_start, 0)
+        # ax.set_aspect("equal")
 
-        plt.show()
+        # plt.show()
 
 
     
