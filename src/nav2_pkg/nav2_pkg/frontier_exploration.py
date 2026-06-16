d5767dff0794d93932764c4d748ade6426a1b13a
Jay Gajjar <jaygajjar890@gmail.com>
2026-06-16 22:46:14 +0530

    added 4 sides frontier clustering logic

--- src/nav2_pkg/nav2_pkg/frontier_exploration.py
+++ src/nav2_pkg/nav2_pkg/frontier_exploration.py
@@ -1,13 +1,22 @@
 import rclpy
-import matplotlib.pyplot as plt
 from rclpy.node import Node
 from nav_msgs.msg import OccupancyGrid
+from collections import deque
+from tf2_ros import Buffer,TransformListener
 
 class frontier_exploration(Node):
     def __init__(self):
         super().__init__("frontier_exploration")
         
         self.map_subscriber_=self.create_subscription(OccupancyGrid,"/map",self.map_callback,10)
+        self.tf_buffer=Buffer()
+        self.tf_listener=TransformListener(self.tf_buffer,self)
+        self.queue=deque()
+        self.visited={}
+        self.current=[]
+        self.cluster=[]
+        self.cluster_main=[]
+
 
     def map_callback(self,msg:OccupancyGrid):
         self.grid_values=msg.data
@@ -17,8 +26,19 @@ class frontier_exploration(Node):
         self.height=msg.info.height
         print(f"height:{self.height}")
         print(len(msg.data)%2)
+        self.resolution=msg.info.resolution
+        self.origin_x=msg.info.origin.position.x
+        self.origin_y=msg.info.origin.position.y
+        print(f"resolution:{self.resolution} origin_x:{self.origin_x} origin_y:{self.origin_y}")
 
-
+        pose=self.get_robot_pose()
+        if pose is not None:
+            robot_x,robot_y=pose
+            print(f"robot x={robot_x}")
+            print(f"robot y={robot_y}")
+            robot_col=int( (robot_x-self.origin_x)/self.resolution)
+            robot_row=int((robot_y-self.origin_y)/self.resolution)
+            print(robot_row,robot_col)
         self.two_d_grid=[]
         self.temp=[]
         for i in self.grid_values:
@@ -34,46 +54,137 @@ class frontier_exploration(Node):
         print(f"lenght of self.two_f_grid:{len(self.two_d_grid)}")
         print(f"2d grid:{self.two_d_grid}")
 
+        self.frontier_cell_list=[]
 
 
-        # grid = self.two_d_grid
+        width=self.width
+        height=self.height
+        print(f"width,height;{(width,height)}")
 
-        # window = 10
-        # center_row = 50
-        # center_col = 40
+       
+        for r in range(0,height):
+            for c in range(0,width):
 
-        # r_start = max(0, center_row - window)
-        # r_end = min(len(grid), center_row + window + 1)
+                if r-1>=0:
+                    upper=self.two_d_grid[r-1][c]
+                else:
+                    upper=False
 
-        # c_start = max(0, center_col - window)
-        # c_end = min(len(grid[0]), center_col + window + 1)
+                if r+1<height:
+                    lower=self.two_d_grid[r+1][c]
+                else:
+                    lower=False
 
-        # fig, ax = plt.subplots(figsize=(10, 10))
+                if c-1>=0:
+                    left=self.two_d_grid[r][c-1]
+                else:
+                    left=False
 
-        # for r in range(r_start, r_end):
-        #     for c in range(c_start, c_end):
-        #         ax.text(
-        #             c - c_start + 0.5,
-        #             r - r_start + 0.5,
-        #             str(grid[r][c]),
-        #             ha="center",
-        #             va="center"
-        #         )
+                if c+1<width:
+                    right=self.two_d_grid[r][c+1]
+                else: 
+                    right=False
+                
+                if self.two_d_grid[r][c]==0:
+                        
+                    if (upper==-1) or (lower==-1) or (right==-1) or (left==-1):
+                        self.frontier_cell_list.append((r,c))
+                                
+        print(*self.frontier_cell_list,sep="\n")
 
-        # for x in range(c_end - c_start + 1):
-        #     ax.axvline(x)
+        for cell in self.frontier_cell_list:
+         if cell not in self.visited:   
+            
+            # print(f"cell:{cell}")
+            self.queue.append(cell)     
+            self.visited[cell]=True
+            # print(f" before while satrt:{cell} queue:{self.queue} visited {self.visited}")
+            
+         
 
-        # for y in range(r_end - r_start + 1):
-        #     ax.axhline(y)
+            while(self.queue):
+            #    print("enterd while")
+               self.current=self.queue[0]
+               if self.current not in self.cluster:
+                  self.cluster.append(self.current)
+               self.queue.popleft()
+            #    print(f"queue:{self.queue} current:{self.current} visited {self.visited}")
+               
+               
 
-        # ax.set_xlim(0, c_end - c_start)
-        # ax.set_ylim(r_end - r_start, 0)
-        # ax.set_aspect("equal")
+               self.r,self.c=self.current
+               self.lower=(self.r+1,self.c)
+               self.left=(self.r,self.c-1)
+               self.upper=(self.r-1,self.c)
+               self.right=(self.r,self.c+1)
+               
+               
+               if self.lower in self.frontier_cell_list and self.lower not in self.visited:
+                #   print(f"self.lower {self.lower}")
+                  self.queue.append(self.lower)
+                  self.visited[self.lower]=True
+                  self.cluster.append(self.lower)
+                #   print(f"lower queue {self.queue} visited:{self.visited} cluster:{self.cluster}")
+                  
+               
+               if self.upper in self.frontier_cell_list and self.upper not in self.visited :
+                #   print(f"fself.upper {self.upper}")
+                  self.queue.append(self.upper)
+                  self.visited[self.upper]=True
+                  self.cluster.append(self.upper)
+                #   print(f"upper queue {self.queue} visited:{self.visited} cluster:{self.cluster}")
+                  
+               
+               if self.left in self.frontier_cell_list and self.left not in self.visited:
+                #   print(f"self.left {self.left}")
+                  self.queue.append(self.left)
+                  self.visited[self.left]=True
+                  self.cluster.append(self.left)
+                #   print(f"left queue {self.queue} visited:{self.visited} cluster:{self.cluster}")
+                  
+               
+               if self.right in self.frontier_cell_list and self.right not in self.visited:
+                #   print(f"self.right {self.right}")
+                  self.queue.append(self.right)
+                  self.visited[self.right]=True
+                  self.cluster.append(self.right)
+                #   print(f"right queue {self.queue} visited:{self.visited} cluster:{self.cluster}")
+                  
+               
+            self.cluster_main.append(self.cluster)
+            self.cluster=[]
+            # print(f"OUT OF WHILE LOOP self.cluster {self.cluster} visited {self.visited} queue:{self.queue}")
+        print(f"main clustor: {self.cluster_main}")
 
-        # plt.show()
+                
 
 
-    
+                            
+
+
+                
+                        
+        # print("\n".join(selected_list))
+            
+
+
+
+
+    def get_robot_pose(self):
+        try:
+            transform=self.tf_buffer.lookup_transform(
+                'map',
+                'base_link',
+                rclpy.time.Time()
+            )
+            x=transform.transform.translation.x
+            y=transform.transform.translation.y
+            return x,y
+        except Exception as e:
+            self.get_logger().warn(str(e))
+            return None
+
+
 
 
 
