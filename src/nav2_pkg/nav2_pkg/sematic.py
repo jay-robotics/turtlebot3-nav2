d2d0b6c7e2d38d6ff15f4edb8e3316cce85bfa7d
Jay Gajjar <jaygajjar890@gmail.com>
2026-07-01 18:49:14 +0530

    added lidar rays visual on opencv frame, and centre point on boxes

--- src/nav2_pkg/nav2_pkg/sematic.py
+++ src/nav2_pkg/nav2_pkg/sematic.py
@@ -4,16 +4,21 @@ from cv_bridge import CvBridge
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 import rclpy
+import math
+from sensor_msgs.msg import LaserScan
+from message_filters import Subscriber
+from message_filters import ApproximateTimeSynchronizer
 
 
 class sematic_mapping(Node):
         def __init__(self):
             super().__init__("sematic_mapping")
 
-            self.image_subscriber=self.create_subscription(Image,"/camera/image_raw",self.callback,10)
+            self.image_subscriber=Subscriber(Image,"/camera/image_raw",self.callback,10)
             self.bridge=CvBridge()  #converts ros image type to opencv image type(numpy array) , self.bridge is an object of CvBridge class whicih contains several functions for converting images
             self.model=YOLO("yolo11x.pt") #loads trained neural network
             self.obj_list=[]
+            self.scan_subscriber_=self.Subscriber(LaserScan,"/scan",)
 
 
 
@@ -24,11 +29,18 @@ class sematic_mapping(Node):
                 # Run YOLO
             results = self.model(self.frame,conf=0.8)  #this variable stores the detected objects
 
+
             # Get the first (and only) result
             # print(results)
 
             for result in results:
+                annotated_frame = result.plot()
+
                 for box in result.boxes:
+                    x1,y1,x2,y2=box.xyxy[0] #when detects it returns bounding box as box.xyxy=(x1,y1,x2,y2),(x1,y1)= top left corner ,(x2,y2)=botton right corner
+                    centrex=int((x1+x2)/2)
+                    centrey=int((y1+y2)/2)
+                    cv2.circle(annotated_frame,(centrex,centrey),5,(0,0,255),-1)  #cv2.circle(image, centre, radius, color(B,G,R), thickness(2=draws only border, -1=fills the circle))
                     class_id = int(box.cls[0])
                     object_name = result.names[class_id]
                     confidence = float(box.conf[0])
@@ -45,8 +57,40 @@ class sematic_mapping(Node):
 
             print(self.obj_list)
             # #Draw bounding boxes and labels on the detected image
-            annotated_frame = result.plot()
+            height,width=annotated_frame.shape[:2]   #finding the size of image(frame),annoted_frame is a numpy array which has .shape property which give (height,width,shape)
+            centre_x=width//2 # // because opencv drawing fns expect integers but / gives decimals
+            centre_y=height//2
+            cv2.line(annotated_frame,(centre_x,0),(centre_x,height),(0,255,0),2)  #cv2.line(image, start_point, end_point, color, thickness)
+            cv2.line(annotated_frame,(0,centre_y),(width,centre_y),(0,255,0),2)
 
+            # height, width = annotated_frame.shape[:2]
+
+            # centre_x = width // 2
+            # centre_y = height // 2
+
+            # Long enough to reach outside the image
+            length = max(width, height) * 2
+
+            for angle in range(360):
+
+                theta = math.radians(angle)
+
+                # Your convention:
+                # 0° = Up
+                # 90° = Left
+                # 180° = Down
+                # 270° = Right
+
+                end_x = int(centre_x - length * math.sin(theta))
+                end_y = int(centre_y - length * math.cos(theta))
+
+                cv2.line(
+                    annotated_frame,
+                    (centre_x, centre_y),
+                    (end_x, end_y),
+                    (255, 255, 255),
+                    1
+                )
             ## displays the annotated image
             cv2.imshow("YOLO Detection", annotated_frame)
             cv2.waitKey(1)
