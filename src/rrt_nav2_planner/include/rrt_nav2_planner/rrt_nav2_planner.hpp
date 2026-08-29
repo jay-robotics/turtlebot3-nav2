a042113709bf88b51b07d2f18c79720626c14663
Jay Gajjar <jaygajjar890@gmail.com>
2026-08-29 10:39:42 +0530

    added header

---
+++ src/rrt_nav2_planner/include/rrt_nav2_planner/rrt_nav2_planner.hpp
@@ -0,0 +1,47 @@
+#ifndef RRT_NAV2_PLANNER__RRT_PLANNER_HPP_
+#define RRT_NAV2_PLANNER__RRT_PLANNER_HPP_
+
+#include <memory>
+#include <string>
+
+#include "nav2_core/global_planner.hpp"
+#include "nav2_costmap_2d/costmap_2d_ros.hpp"
+#include "nav_msgs/msg/path.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+namespace rrt_nav2_planner
+{
+
+class RRTPlanner : public nav2_core::GlobalPlanner
+{
+public:
+
+  void configure(
+    const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent,
+    std::string name,
+    std::shared_ptr<tf2_ros::Buffer> tf,
+    std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros) override;
+
+  void cleanup() override;
+
+  void activate() override;
+
+  void deactivate() override;
+
+  nav_msgs::msg::Path createPlan(
+    const geometry_msgs::msg::PoseStamped & start,
+    const geometry_msgs::msg::PoseStamped & goal) override;
+
+private:
+
+  rclcpp_lifecycle::LifecycleNode::WeakPtr node_;
+  std::string name_;
+
+  std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros_;
+
+};
+
+}  // namespace rrt_nav2_planner
+
+#endif  // RRT_NAV2_PLANNER__RRT_PLANNER_HPP_
